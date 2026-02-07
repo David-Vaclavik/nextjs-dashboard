f50b53ad3a48fed8cf8dc13bcfa729b3509572bf
David-Vaclavik <davidvaclavik.dv@gmail.com>
2026-02-07 12:08:12 +0100

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import "@/app/ui/global.css"; // Import global CSS styles in root layout, cannot be imported in components
+import { inter, outfit } from "./ui/fonts";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+      {/* <body className={`${outfit.className} antialiased`}>{children}</body> */}
+      {/* <body>{children}</body> */}
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Outfit, Lusitana, Geist } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin-ext"] });
+export const outfit = Outfit({ subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] });
+export const geist = Geist({ subsets: ["latin"] });
