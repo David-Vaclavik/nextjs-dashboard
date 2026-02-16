e111645814824315c0ac4be7eae6479135fec32e
David-Vaclavik <davidvaclavik.dv@gmail.com>
2026-02-16 18:56:45 +0100

    feat: Chapter 9 done

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+// Chapter 9: Streaming and Suspense
+// https://nextjs.org/learn/dashboard-app/streaming
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-groups
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,7 +1,8 @@
 import { generateYAxis } from "@/app/lib/utils";
 import { CalendarIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
-import { Revenue } from "@/app/lib/definitions";
+// import { Revenue } from "@/app/lib/definitions";
+import { fetchRevenue } from "@/app/lib/data";
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -10,7 +11,9 @@ import { Revenue } from "@/app/lib/definitions";
 // https://airbnb.io/visx/
 
 //! Uncommented in Chapter 7
-export default async function RevenueChart({ revenue }: { revenue: Revenue[] }) {
+export default async function RevenueChart() {
+  //! added in Chapter 9
+  const revenue = await fetchRevenue();
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
